e08dd4fb07737b50fa6f918b551d57389a28f6f8
user.name: Personal <user.email: reniejr@gmail.com>
2022-08-11 14:51:44 +0200

    feat: product filo-tag added

---
+++ products/filo-tag.js
@@ -0,0 +1,18 @@
+const filo_tag_x1 = {
+    "bundle": "single",
+    "name": "1x Filo Tag",
+    "variants": ["red", "black", "blue", "white"],
+    "price": "24,90 €"
+}
+
+const filo_tag_x4 = {
+    "bundle": "x4",
+    "name": "4x Filo Tag",
+    "variants": ["red", "black", "blue", "white", "mix"],
+    "price": "64,90 €"
+}
+
+export {
+    filo_tag_x1,
+    filo_tag_x4
+}
